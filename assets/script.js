ab40fd0632626ec2ef15d48f28b5656feaee3f5b
Fabrizio Maia <fabrizio.apparicio@gmail.com>
2025-11-11 17:46:02 -0300

    Nova alteração no script

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
 document.getElementById('formPresenca').addEventListener('submit', function(e) {
-      e.preventDefault();
-      const nomes = document.getElementById('nomes').value;
+  e.preventDefault();
+  const nomes = document.getElementById('nomes').value;
 
-      fetch('https://script.google.com/macros/s/AKfycbxFPZv5T8TRMYkoCx35_l9R5a0_qVBnSN2XC4wUksYe4Ou-ib55k7AopPAcozczWFcI1w/exec', {
-        method: 'POST',
-        body: new URLSearchParams({ nomes })
-      })
-      .then(response => response.text())
-      .then(data => {
-        alert('Presença confirmada com sucesso! 🎉');
-        document.getElementById('formPresenca').reset();
-      })
-      .catch(error => alert('Erro ao confirmar presença. Tente novamente.'));
-    });
+  fetch('https://script.google.com/macros/s/AKfycbxFPZv5T8TRMYkoCx35_l9R5a0_qVBnSN2XC4wUksYe4Ou-ib55k7AopPAcozczWFcI1w/exec', {
+    method: 'POST',
+    body: new URLSearchParams({ nomes: nomes })
+  })
+  .then(response => response.text())
+  .then(data => {
+    alert('Presença confirmada com sucesso! 🎉');
+    document.getElementById('formPresenca').reset();
+  })
+  .catch(error => {
+    alert('Erro ao confirmar presença. Verifique a conexão.');
+    console.error(error);
+  });
+});
